4b8f93e3d2b30613b979cffa74a02d3adfb886d4
mmaranda-cs <mmaranda@camsys.com>
2017-01-03 16:07:07 -0500

    re-removing funding from accounting without gutting everything else.

--- lib/transam_funding/funding_funding_source.rb
+++ lib/transam_funding/funding_funding_source.rb
@@ -4,4 +4,17 @@ module FundingFundingSource
   has_many    :funding_templates, :dependent => :destroy
   has_many    :funding_buckets, :through => :funding_templates
 
+
+  #------------------------------------------------------------------------------
+  #
+  # Instance Methods
+  #
+  #------------------------------------------------------------------------------
+
+  def deleteable?
+
+    # any bucket/grant must be associated with a template and therefore only need to check template count
+    funding_templates.count == 0
+  end
+
 end
