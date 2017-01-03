5ed102ea26f2dc3ca2a1cb5f8ed95d54561c3cb7
James Brooks <jbrooks@camsys.com>
2017-01-03 17:34:09 -0500

    Add missing include block in FundingFundingSource extension

--- lib/transam_funding/funding_funding_source.rb
+++ lib/transam_funding/funding_funding_source.rb
@@ -1,8 +1,10 @@
 module FundingFundingSource
   extend ActiveSupport::Concern
 
-  has_many    :funding_templates, :dependent => :destroy
-  has_many    :funding_buckets, :through => :funding_templates
+  included do
+    has_many    :funding_templates, :dependent => :destroy
+    has_many    :funding_buckets, :through => :funding_templates
+  end
 
 
   #------------------------------------------------------------------------------
