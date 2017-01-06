979776a4ab579bc14bb4ff296bbaab42087492a9
Lydia Chang <mathmerized@gmail.com>
2017-01-06 10:27:51 -0500

    require mixin

--- lib/transam_funding.rb
+++ lib/transam_funding.rb
@@ -1,6 +1,7 @@
 require "transam_funding/engine"
 require "transam_funding/transam_fundable"
 require "transam_funding/transam_fundable_capital_project"
+require "transam_funding/funding_funding_source"
 
 module TransamFunding
 end
