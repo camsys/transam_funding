ceb65716ce22493f00611ea904722b8b2da15777
mmaranda-cs <mmaranda@camsys.com>
2017-04-25 13:47:45 -0400

    [#144239475] allows funding requests up to 1,000,000,000,000 this also means that any single line federal, state, or local could be that large.

---
+++ db/migrate/201704205131242_update_funding_request_precision.rb
@@ -0,0 +1,16 @@
+class UpdateFundingRequestPrecision < ActiveRecord::Migration
+  def change
+    reversible do |dir|
+      change_table :funding_requests do |t|
+        dir.up   { t.change :federal_amount, :int8, :limit => 8  }
+        dir.down { t.change :federal_amount, :integer }
+        dir.up   { t.change :state_amount, :int8, :limit => 8  }
+        dir.down { t.change :state_amount, :integer }
+        dir.up   { t.change :local_amount, :int8, :limit => 8  }
+        dir.down { t.change :local_amount, :integer }
+        dir.up   { t.change :funding_request_amount, :int8, :limit => 8  }
+        dir.down { t.change :funding_request_amount, :integer }
+      end
+    end
+  end
+end
