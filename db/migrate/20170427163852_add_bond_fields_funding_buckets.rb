5579de4163706913c8bb42ce597403ed764c96d2
Lydia Chang <mathmerized@gmail.com>
2017-04-28 15:21:43 -0400

    [#131804973] add additional fields for bond authorization buckets

---
+++ db/migrate/20170427163852_add_bond_fields_funding_buckets.rb
@@ -0,0 +1,10 @@
+class AddBondFieldsFundingBuckets < ActiveRecord::Migration
+  def change
+    add_column :funding_buckets, :line_num, :string
+    add_column :funding_buckets, :act_num, :string
+    add_column :funding_buckets, :pt_num, :string
+    add_column :funding_buckets, :grantee_code, :string
+    add_column :funding_buckets, :page_num, :string
+    add_column :funding_buckets, :item_num, :string
+  end
+end
