d7846d773d4e89eb22490a244c4f2829cc6bcd09
W. Scott Meeks <smeeks@camsys.com>
2017-06-06 13:43:14 -0400

    Forgot Fiscal Year.

--- app/reports/needs_funding_report.rb
+++ app/reports/needs_funding_report.rb
@@ -7,11 +7,11 @@ class NeedsFundingReport < AbstractReport
   def get_data(organization_id_list, params)
     # Assumption: data values have already been divided by 10^6 when returned.
 
-    labels = ['Total Needs ($M)', 'Total Federal Funds ($M)', 'Total State Funds ($M)',
+    labels = ['Fiscal Year', 'Total Needs ($M)', 'Total Federal Funds ($M)', 'Total State Funds ($M)',
               'Total Local Funds ($M)', 'Balance/(Shortfall) ($M)']
-    formats = [:currency, :currency, :currency, :currency, :currency]
+    formats = [nil, :currency, :currency, :currency, :currency, :currency]
 
-    data = [[987.654321, 456.0, 321.0, 123.0, 900.654321]]
+    data = [['FY 17-18', 987.654321, 456.0, 321.0, 123.0, -900.654321]]
 
     return {labels: labels, data: data, formats: formats}
   end
