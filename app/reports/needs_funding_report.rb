5d1d64ad6bf28243da93887c0ebfe5fb05d3486b
Lydia Chang <mathmerized@gmail.com>
2017-06-07 13:59:11 -0400

    [fixes#145004935] need v funding report and rake task to add funding reports

--- app/reports/needs_funding_report.rb
+++ app/reports/needs_funding_report.rb
@@ -1,17 +1,38 @@
 class NeedsFundingReport < AbstractReport
 
+  include FiscalYear
+
   def initialize(attributes = {})
     super(attributes)
   end    
   
   def get_data(organization_id_list, params)
-    # Assumption: data values have already been divided by 10^6 when returned.
+
+
+    fiscal_years = get_fiscal_years
+
+        # Assumption: data values have already been divided by 10^6 when returned.
+    needs = ActivityLineItem.unscoped.order("activity_line_items.fy_year").group("activity_line_items.fy_year").sum(ActivityLineItem::COST_SUM_SQL_CLAUSE)
+
+    total_funds = FundingBucket.where('fy_year > ?', current_planning_year_year).order(:fy_year).group(:fy_year).sum(:budget_amount)
+    fed_funds = FundingBucket.federal.where('fy_year > ?', current_planning_year_year).order(:fy_year).group(:fy_year).sum('budget_amount/1000000')
+    state_funds = FundingBucket.state.where('fy_year > ?', current_planning_year_year).order(:fy_year).group(:fy_year).sum('budget_amount/1000000')
+    local_funds = FundingBucket.local.where('fy_year > ?', current_planning_year_year).order(:fy_year).group(:fy_year).sum('budget_amount/1000000')
+
+    total_funds[current_planning_year_year] = FundingBucket.current(current_planning_year_year).sum(:budget_amount)
+    fed_funds[current_planning_year_year] = (FundingBucket.federal.current(current_planning_year_year).sum(:budget_amount)/1000000.0).round(2)
+    state_funds[current_planning_year_year] = (FundingBucket.state.current(current_planning_year_year).sum(:budget_amount)/1000000.0).round(2)
+    local_funds[current_planning_year_year] = (FundingBucket.local.current(current_planning_year_year).sum(:budget_amount)/1000000.0).round(2)
 
     labels = ['Fiscal Year', 'Total Needs ($M)', 'Total Federal Funds ($M)', 'Total State Funds ($M)',
               'Total Local Funds ($M)', 'Balance/(Shortfall) ($M)']
     formats = [nil, :currency, :currency, :currency, :currency, :currency]
 
-    data = [['FY 17-18', 987.654321, 456.0, 321.0, 123.0, -900.654321]]
+    data = []
+    fiscal_years.each do |fy|
+      fy_need = (needs[fy[1]] || 0)
+      data << [fy[0], (fy_need/ 1000000.0).round(2), fed_funds[fy[1]] || 0, state_funds[fy[1]] || 0, local_funds[fy[1]] || 0, (((total_funds[fy[1]] || 0) - fy_need)/1000000.0).round(2)]
+    end
 
     return {labels: labels, data: data, formats: formats}
   end
--- lib/tasks/transam.rake
+++ lib/tasks/transam.rake
@@ -72,4 +72,34 @@ namespace :transam do
       end
     end
   end
+
+  desc "Add funding reports"
+  task add_funding_reports: :environment do
+    reports = [
+        {
+            :active => 1,
+            :belongs_to => 'report_type',
+            :type => "Capital Needs Report",
+            :name => 'Needs Versus Funding Report',
+            :class_name => "NeedsFundingReport",
+            :view_name => "generic_formatted_table",
+            :show_in_nav => 1,
+            :show_in_dashboard => 1,
+            :printable => true,
+            :exportable => true,
+            :roles => 'guest,user,manager',
+            :description => 'Displays a report showing the total needs by fiscal year versus available funding, broken out by source of funds.',
+            :chart_type => '',
+            :chart_options => ""
+        }
+    ]
+
+    reports.each do |row|
+      if Report.find_by(class_name: row[:class_name]).nil?
+        x = Report.new(row.except(:belongs_to, :type))
+        x.report_type = ReportType.where(:name => row[:type]).first
+        x.save!
+      end
+    end
+  end
 end
