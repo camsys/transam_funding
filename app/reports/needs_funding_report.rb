daf4bf421c1678aa2b8144e8d2e515e3322a1fc3
Lydia Chang <lydiachang@CHANGL-MAC.local>
2017-06-26 07:40:02 -0400

    needs vs funding report cleanup, no $ in M, and statewide label

--- app/reports/needs_funding_report.rb
+++ app/reports/needs_funding_report.rb
@@ -15,23 +15,23 @@ class NeedsFundingReport < AbstractReport
     needs = ActivityLineItem.unscoped.order("activity_line_items.fy_year").group("activity_line_items.fy_year").sum(ActivityLineItem::COST_SUM_SQL_CLAUSE)
 
     total_funds = FundingBucket.where('fy_year > ?', current_planning_year_year).order(:fy_year).group(:fy_year).sum(:budget_amount)
-    fed_funds = FundingBucket.federal.where('fy_year > ?', current_planning_year_year).order(:fy_year).group(:fy_year).sum('budget_amount/1000000')
-    state_funds = FundingBucket.state.where('fy_year > ?', current_planning_year_year).order(:fy_year).group(:fy_year).sum('budget_amount/1000000')
-    local_funds = FundingBucket.local.where('fy_year > ?', current_planning_year_year).order(:fy_year).group(:fy_year).sum('budget_amount/1000000')
+    fed_funds = FundingBucket.federal.where('fy_year > ?', current_planning_year_year).order(:fy_year).group(:fy_year).sum(:budget_amount)
+    state_funds = FundingBucket.state.where('fy_year > ?', current_planning_year_year).order(:fy_year).group(:fy_year).sum(:budget_amount)
+    local_funds = FundingBucket.local.where('fy_year > ?', current_planning_year_year).order(:fy_year).group(:fy_year).sum(:budget_amount)
 
     total_funds[current_planning_year_year] = FundingBucket.current(current_planning_year_year).sum(:budget_amount)
-    fed_funds[current_planning_year_year] = (FundingBucket.federal.current(current_planning_year_year).sum(:budget_amount)/1000000.0).round(2)
-    state_funds[current_planning_year_year] = (FundingBucket.state.current(current_planning_year_year).sum(:budget_amount)/1000000.0).round(2)
-    local_funds[current_planning_year_year] = (FundingBucket.local.current(current_planning_year_year).sum(:budget_amount)/1000000.0).round(2)
+    fed_funds[current_planning_year_year] = FundingBucket.federal.current(current_planning_year_year).sum(:budget_amount)
+    state_funds[current_planning_year_year] = FundingBucket.state.current(current_planning_year_year).sum(:budget_amount)
+    local_funds[current_planning_year_year] = FundingBucket.local.current(current_planning_year_year).sum(:budget_amount)
 
-    labels = ['Fiscal Year', 'Total Needs ($M)', 'Total Federal Funds ($M)', 'Total State Funds ($M)',
-              'Total Local Funds ($M)', 'Balance/(Shortfall) ($M)']
-    formats = [nil, :currencyM, :currencyM, :currencyM, :currencyM, :currencyM]
+    labels = ['Fiscal Year', 'Total Needs', 'Total Federal Funds', 'Total State Funds',
+              'Total Local Funds', 'Balance/(Shortfall)']
+    formats = [nil, :currency, :currency, :currency, :currency, :currency]
 
     data = []
     fiscal_years.each do |fy|
       fy_need = (needs[fy[1]] || 0)
-      data << [fy[0], (fy_need/ 1000000.0).round(2), fed_funds[fy[1]] || 0, state_funds[fy[1]] || 0, local_funds[fy[1]] || 0, (((total_funds[fy[1]] || 0) - fy_need)/1000000.0).round(2)]
+      data << [fy[0], fy_need, fed_funds[fy[1]] || 0, state_funds[fy[1]] || 0, local_funds[fy[1]] || 0, (total_funds[fy[1]] || 0) - fy_need]
     end
 
     return {labels: labels, data: data, formats: formats}
