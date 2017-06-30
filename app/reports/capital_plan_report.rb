55f43035d1452f6a0391d3300c9247041bb1b6e5
W. Scott Meeks <smeeks@camsys.com>
2017-06-30 13:27:48 -0400

    Various fixes to Capital Plan report. [#143092125]
    Add From/To labels to filter.
    Fix Scope format.
    Add ALI count.
    Sort by org then FY.
    Default filter to current year.

--- app/reports/capital_plan_report.rb
+++ app/reports/capital_plan_report.rb
@@ -11,52 +11,65 @@ class CapitalPlanReport < AbstractReport
       {
         type: :select,
         where: :start_fy_year,
-        values: get_fiscal_years
+        values: get_fiscal_years,
+        label: 'From'
       },
       {
         type: :select,
         where: :end_fy_year,
-        values: get_fiscal_years
+        values: get_fiscal_years,
+        label: 'To'
       }
     ]
   end
   
   def get_data(organization_id_list, params)
 
-    labels = ['FY', 'Project', 'Title', 'Scope', 'Cost', 'Fed $', 'State $', 'Local $']
-    formats = [:fiscal_year, nil, nil, nil, :currency, :currency, :currency, :currency]
+    labels = ['FY', 'Project', 'Title', 'Scope', '# ALIs', 'Cost', 'Fed $', 'State $', 'Local $']
+    formats = [:fiscal_year, :string, :string, :string, :integer, :currency, :currency, :currency, :currency]
     
-    # Default scope orders by project_id
-    query = CapitalProject.where(organization_id: organization_id_list)
+    # Order by org name, then by FY
+    query = CapitalProject.where(organization_id: organization_id_list).joins(:organization).order('organizations.name', :fy_year)
 
     # Add clauses based on params
     conditions = []
     values = []
-    if params[:start_fy_year].present?
-      conditions << 'capital_projects.fy_year >= ?'
-      values << params[:start_fy_year].to_i 
-    end
 
-    if params[:end_fy_year].present?
-      conditions << 'capital_projects.fy_year <= ?'
-      values << params[:end_fy_year].to_i 
-    end
+    value = params[:start_fy_year] || current_planning_year_year
+    conditions << 'capital_projects.fy_year >= ?'
+    values << value.to_i 
+    
+    value = params[:end_fy_year] || current_planning_year_year
+    conditions << 'capital_projects.fy_year <= ?'
+    values << value.to_i 
 
     query = query.where(conditions.join(' AND '), *values).eager_load(:activity_line_items, :funding_requests)
     
     data = []
+    org_data = []
+    current_org = nil
     query.each do |cp|
-      data << [
+      row = [
         cp.fy_year,
         cp.project_number,
         cp.title,
         cp.team_ali_code.scope,
+        cp.activity_line_items.count,
         cp.total_cost,
         cp.federal_funds,
         cp.state_funds,
         cp.local_funds
       ]
+      if current_org != cp.organization
+        data << [current_org.name, org_data] if current_org
+        org_data = [row]
+        current_org = cp.organization
+      else
+        org_data << row
+      end
     end
+    data << [current_org.name, org_data]
+    
     return {labels: labels, data: data, formats: formats}
   end
 
