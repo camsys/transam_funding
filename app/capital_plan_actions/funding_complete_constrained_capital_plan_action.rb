9ce816f447ef6dda5d4133e1abea59978a1b2719
mathmerized <mathmerized@gmail.com>
2017-10-09 12:28:36 -0400

    syntax errors on counting overcommitted buckets

--- app/capital_plan_actions/funding_complete_constrained_capital_plan_action.rb
+++ app/capital_plan_actions/funding_complete_constrained_capital_plan_action.rb
@@ -16,7 +16,7 @@ class FundingCompleteConstrainedCapitalPlanAction < BaseCapitalPlanAction
     pcnt_funded = ali_count > 0 ? (funded_ali_count * 100.0 / ali_count).to_i : 100
 
     FundingRequest.where(activity_line_item_id: alis.ids).pluck(:federal_funding_line_item_id, :state_funding_line_item_id, :local_funding_line_item_id).flatten.uniq
-    overcommitted_buckets_count = FundingBucket.where(id: FundingRequest.joins(activity_line_item: :capital_project).where('capital_projects.organization_id IN (?)', @organization_list).pluck(:federal_funding_line_item_id, :state_funding_line_item_id, :local_funding_line_item_id).flatten.uniq).where('budget_committed > budget_amount').count
+    overcommitted_buckets_count = FundingBucket.where(id: FundingRequest.joins(activity_line_item: :capital_project).where('capital_projects.organization_id = ?', capital_plan.organization_id).pluck(:federal_funding_line_item_id, :state_funding_line_item_id, :local_funding_line_item_id).flatten.uniq).where('budget_committed > budget_amount').count
     if overcommitted_buckets_count > 0
       if @user.organization.organization_type.class_name == 'Grantor'
         url = Rails.application.routes.url_helpers.funding_buckets_url(funds_filter: 'funds_overcommitted')
@@ -36,7 +36,7 @@ class FundingCompleteConstrainedCapitalPlanAction < BaseCapitalPlanAction
   end
 
   def post_process
-    overcommitted_buckets_count = FundingBucket.where(id: FundingRequest.where(activity_line_item_id: alis.ids).pluck(:federal_funding_line_item_id, :state_funding_line_item_id, :local_funding_line_item_id).flatten.uniq).where('budget_committed > budget_amount').count
+    overcommitted_buckets_count = FundingBucket.where(id: FundingRequest.joins(activity_line_item: :capital_project).where('capital_projects.organization_id = ?', @capital_plan_action.capital_plan.organization_id).pluck(:federal_funding_line_item_id, :state_funding_line_item_id, :local_funding_line_item_id).flatten.uniq).where('budget_committed > budget_amount').count
     if @capital_plan_action.completed_pcnt == 100 && overcommitted_buckets_count == 0
       super
     end
