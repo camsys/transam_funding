3cf231243dbaa6acf1a979fa94ec0bf67406814c
mathmerized <mathmerized@gmail.com>
2018-06-07 16:25:39 -0400

    fix mixins accidentally put in cpt that should be in funding

--- db/data_migrations/20180605201144_add_funding_system_config_extensions.rb
+++ db/data_migrations/20180605201144_add_funding_system_config_extensions.rb
@@ -1,7 +1,9 @@
 class AddFundingSystemConfigExtensions < ActiveRecord::DataMigration
   def up
     system_config_extensions = [
-        {class_name: 'FundingSource', extension_name: 'FundingFundingSource', active: true}
+        {class_name: 'FundingSource', extension_name: 'FundingFundingSource', active: true},
+        {class_name: 'CapitalProject', extension_name: 'TransamFundableCapitalProject', active: true},
+        {class_name: 'ActivityLineItem', extension_name: 'TransamFundable', active: true}
 
     ]
 
