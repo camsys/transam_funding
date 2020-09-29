c9794c3e492f5c879966be79b3b8a259ec8515ca
Derek Edwards <dedwards8@gmail.com>
2020-09-29 15:24:27 -0400

    Update migration for creating a created_by_user_id

--- db/migrate/20200929003904_add_created_by_to_funding_template.rb
+++ db/migrate/20200929003904_add_created_by_to_funding_template.rb
@@ -1,6 +1,7 @@
-class AddCreatedByToFundingTemplate < ActiveRecord::Migration[5.2]
+class AddCreatedByToFundingTemplate < ActiveRecord::Migration[5.0]
   def change
-    add_column :funding_templates, :created_by_user_id, :bigint
-     add_foreign_key :funding_templates, :users, column: :created_by_user_id, primary_key: :id
+    #add_column :funding_templates, :created_by_user_id, :bigint
+    #add_foreign_key :funding_templates, :users, column: :created_by_user_id, primary_key: :id
+    add_reference :funding_templates, :created_by_user, foreign_key: { to_table: :users }
   end
 end
