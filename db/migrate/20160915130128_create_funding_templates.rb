cd97b8d867d8370b55ac458d9e777f931e46ef48
mathmerized <mathmerized@gmail.com>
2020-09-29 14:31:55 -0400

    TTPLAT-1780 more migration cleanup

--- db/migrate/20160915130128_create_funding_templates.rb
+++ db/migrate/20160915130128_create_funding_templates.rb
@@ -1,49 +1,52 @@
 class CreateFundingTemplates < ActiveRecord::Migration[4.2]
   def change
-    create_table :funding_templates do |t|
-      t.string :object_key,         index: true, limit: 12, null: false
-      t.references :funding_source, index: true
-      t.string :name,               limit: 64, null: false
-      t.text :description
-      t.references :contributer,    index: true, null: false
-      t.references :owner,          index: true, null: false
-      t.boolean :recurring
-      t.boolean :transfer_only
-      t.float :match_required
-      t.boolean :active,            null: false
+    # this is an informal way to check if already have funding engine
+    unless ActiveRecord::Base.connection.table_exists? :funding_templates
+      create_table :funding_templates do |t|
+        t.string :object_key,         index: true, limit: 12, null: false
+        t.references :funding_source, index: true
+        t.string :name,               limit: 64, null: false
+        t.text :description
+        t.references :contributer,    index: true, null: false
+        t.references :owner,          index: true, null: false
+        t.boolean :recurring
+        t.boolean :transfer_only
+        t.float :match_required
+        t.boolean :active,            null: false
 
-      t.timestamps
-    end
+        t.timestamps
+      end
 
-    create_table :funding_template_types do |t|
-      t.references :funding_source, index: true
-      t.string :name,               limit: 64, null: false
-      t.string :description,        limit: 256, null: false
-      t.boolean :active,            null:false
-    end
+      create_table :funding_template_types do |t|
+        t.references :funding_source, index: true
+        t.string :name,               limit: 64, null: false
+        t.string :description,        limit: 256, null: false
+        t.boolean :active,            null:false
+      end
 
-    create_table :funding_templates_funding_template_types, :id => false do |t|
-      t.references :funding_template
-      t.references :funding_template_type
-    end
+      create_table :funding_templates_funding_template_types, :id => false do |t|
+        t.references :funding_template
+        t.references :funding_template_type
+      end
 
-    add_index :funding_templates_funding_template_types, :funding_template_id, name: 'funding_templates_funding_template_types_idx1'
-    add_index :funding_templates_funding_template_types, :funding_template_type_id, name: 'funding_templates_funding_template_types_idx2'
+      add_index :funding_templates_funding_template_types, :funding_template_id, name: 'funding_templates_funding_template_types_idx1'
+      add_index :funding_templates_funding_template_types, :funding_template_type_id, name: 'funding_templates_funding_template_types_idx2'
 
-    create_table :funding_templates_organizations, :id => false do |t|
-      t.references :funding_template,       index: true
-      t.references :organization,           index: true
-    end
+      create_table :funding_templates_organizations, :id => false do |t|
+        t.references :funding_template,       index: true
+        t.references :organization,           index: true
+      end
 
-    # add seed data
-    funding_template_types = [
-        {:active => 1, :name => 'Capital', :description => 'Capital Funding Template'},
-        {:active => 1, :name => 'Operating', :description => 'Operating Funding Template'},
-        {:active => 1, :name => 'Debt Service', :description => 'Debt Service Funding Template'},
-        {:active => 1, :name => 'Other', :description => 'Other Funding Template'},
-    ]
-    funding_template_types.each do |type|
-      FundingTemplateType.create(type)
+      # add seed data
+      funding_template_types = [
+          {:active => 1, :name => 'Capital', :description => 'Capital Funding Template'},
+          {:active => 1, :name => 'Operating', :description => 'Operating Funding Template'},
+          {:active => 1, :name => 'Debt Service', :description => 'Debt Service Funding Template'},
+          {:active => 1, :name => 'Other', :description => 'Other Funding Template'},
+      ]
+      funding_template_types.each do |type|
+        FundingTemplateType.create(type)
+      end
     end
   end
 end
--- db/migrate/20160919181214_cleanup_funding_tables.rb
+++ db/migrate/20160919181214_cleanup_funding_tables.rb
@@ -1,7 +1,11 @@
 class CleanupFundingTables < ActiveRecord::Migration[4.2]
   def change
-    rename_column :funding_templates, :contributer_id, :contributor_id
-    add_column :funding_templates, :all_organizations, :boolean
-    add_column :funding_templates, :external_id, :string, :limit => 32
+    unless column_exists? :funding_templates, :contributor_id
+      rename_column :funding_templates, :contributer_id, :contributor_id
+    end
+    unless column_exists? :funding_templates, :all_organizations
+      add_column :funding_templates, :all_organizations, :boolean
+      add_column :funding_templates, :external_id, :string, :limit => 32
+    end
   end
 end
