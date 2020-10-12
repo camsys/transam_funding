7c7388f15e0a3f1c1f6a624adcdb61e7ef42a444
mathmerized <mathmerized@gmail.com>
2020-10-12 09:54:21 -0400

    undo removing field needed to get orgs of funding templates

--- db/migrate/20201008155802_remove_booleans_funding_templates.rb
+++ db/migrate/20201008155802_remove_booleans_funding_templates.rb
@@ -1,6 +1,6 @@
 class RemoveBooleansFundingTemplates < ActiveRecord::Migration[5.2]
   def change
-    remove_column :funding_templates, :query_string
+    #remove_column :funding_templates, :query_string
     remove_column :funding_templates, :create_multiple_agencies
     remove_column :funding_templates, :create_multiple_buckets_for_agency_year
   end
