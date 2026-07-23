5368469ad12f8b1937f67f100972f7aae15adb63
aandrade <aandrade@camsys.com>
2026-07-23 18:18:36 -0400

    [TTPLAT-2734] Catch divide by zero case when ALI cost is 0

--- app/reports/scenario_budget_report.rb
+++ app/reports/scenario_budget_report.rb
@@ -27,7 +27,7 @@ class ScenarioBudgetReport < AbstractReport
       row = [
         allocation.draft_project_phase.name,
         allocation.draft_budget.name,
-        format_as_percentage(100*(allocation.amount.to_f/allocation.draft_project_phase.cost), 3),
+        format_as_percentage(allocation.draft_project_phase.cost == 0 ? 0 : 100*(allocation.amount.to_f/allocation.draft_project_phase.cost), 3),
         allocation.amount
       ]
       # When current data is for a new project
